3ae7b78bc65375fa44fac54e581023466e9de1fb
pc <pc@DESKTOP-KJBOUAL>
2024-04-20 21:23:19 +0700

    Update

---
+++ n.linh/n.linh.cpp
@@ -0,0 +1,104 @@
+﻿// n.linh.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    /*int soNguyen;
+
+    cout << "nhap So Nguyen : ";
+    cin >> soNguyen;
+
+    if (soNguyen % 2 == 0)
+    {
+        cout << soNguyen << " So chan ";
+    }
+    else
+    {
+        cout << soNguyen << " So le";
+    }*/
+
+ 
+    // yêu cầu ng dùng nhập từ 1 đến 9 và in ra tiếng hán
+
+    //int dichSoRaTiengHan;
+
+    //cout << "Nhap so o day : ";
+    //cin >> dichSoRaTiengHan;
+
+    //switch (dichSoRaTiengHan)
+    //{
+    //case 1: cout << "Nhat" << endl;
+    //    break;
+    //case 2: cout << "Nhi" << endl;
+    //    break;
+    //case 3: cout << "Tam" << endl;
+    //    break;
+    //case 4: cout << "Tu" << endl;
+    //    break;
+    //case 5: cout << "Ngu" << endl;
+    //    break;
+    //case 6: cout << "Luc" << endl;
+    //    break;
+    //case 7: cout << "That" << endl;
+    //    break;
+    //case 8: cout << "Bat" << endl;
+    //    break;
+    //case 9: cout << "Cuu" << endl;
+    //    break;
+    //case 10: cout << "Thap" << endl;
+    //    break;
+    //}
+
+    /*int a, b;
+    cout << "Bai toan : Nhap gia tri a va b sau do in ra gia tri lon nhat" << endl;
+
+    cout << "Nhap gia tri a : ";
+    cin >> a;
+    cout << "Nhap gia tri b : ";
+    cin >> b;*/
+
+    //if (a > b)
+    //{
+    //    cout << "So lon hon la : " << a;  
+    //}
+    //else if (a < b)
+    //{
+    //    cout << "So Nho hon la : " << b;
+    //}
+    //else
+    //{
+    //    cout << "Ca 2 so deu bang nhau";
+    //}
+
+    // câu điều kiện 3 ngôi :
+    /*int c = (a > b) ? a : b;
+    cout << "gia tri lon nhat la : " << c;*/
+
+    // vi du khac : 
+
+    /*string ketqua = (a % 2 == 0) ? "So chan" : "So le";
+    cout << a << "La so" << ketqua << endl;*/
+
+    // ví dụ khác của câu điều kiện 2 ngôi : 
+    // a = a - 10 => a -= 10
+
+    /*int a = 5;
+    int b = 10;
+    int c = 0;
+    
+    c = a++ + ++b + ++a - b-- + ++b;
+    cout << "ket qua : " << c;*/
+
+    // Phép toán tử && : khác =
+
+   
+
+
+
+    return 0;
+}
+
