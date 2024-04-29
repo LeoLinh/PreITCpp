ab50dcb33538582a382f05ead0efe5ccca2c565c
LeoLinh <nnhatlinhh@gmail.com>
2024-04-29 21:46:58 +0700

    update

--- PreIT-6-BaiTap.2/PreIT-6-BaiTap.2.cpp
+++ PreIT-6-BaiTap.2/PreIT-6-BaiTap.2.cpp
@@ -5,29 +5,27 @@
 
 using namespace std;
 
-// Hàm tính ước số chung lớn nhất của hai số nguyên a và b
+
 int timUSCLN(int a, int b) {
-    // Nếu một trong hai số là 0, ước số chung lớn nhất sẽ là số còn lại
+
     if (a == 0)
         return b;
     if (b == 0)
         return a;
 
-    // Nếu một trong hai số là số âm, chuyển đổi thành số dương
     if (a < 0)
         a = -a;
     if (b < 0)
         b = -b;
 
-    // Tiến hành tìm ước số chung lớn nhất
     for (int i = a; i >= 1; i--) 
     {
         if (a % i == 0 && b % i == 0) 
         {
-            return i; // Trả về ước số chung lớn nhất khi tìm thấy
+            return i; 
         }
     }
-    return 1; // Nếu không tìm thấy ước số chung lớn nhất, trả về 1
+    return 1; 
 }
 
 int main()
