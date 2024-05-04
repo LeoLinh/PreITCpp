38480132fe3165f87e1a6527fff565ce53683d7a
LeoLinh <nnhatlinhh@gmail.com>
2024-05-04 14:13:07 +0700

    update

---
+++ PreIT-6-SoNguyenN/PreIT-6-SoNguyenN.cpp
@@ -0,0 +1,48 @@
+﻿// PreIT-6-SoNguyenN.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int N;
+    cout << "Nhap so nguyen N: ";
+    cin >> N;
+
+    // Phép tính 1: Tính tổng các số lẻ đến N
+    int S1 = 0;
+    for (int i = 1; i <= N; i += 2) {
+        S1 += i;
+    }
+
+    // Phép tính 2: Tính tổng các số chẳn đến N
+    int S2 = 0;
+    for (int i = 2; i <= N; i += 2) {
+        S2 += i;
+    }
+
+    // Phép tính 3: Tính tổng bình phương các số đến N
+    int S3 = 0;
+    for (int i = 1; i <= N; i++) {
+        S3 += i * i;
+    }
+
+    // Phép tính 4: Tính tổng các số đến N theo cấu trúc
+    int S4 = 0;
+    int dau = 1;
+    for (int i = 1; i <= N; i++) {
+        S4 += dau * i;
+        dau *= -1; // Đảo dấu sau mỗi lần lặp
+    }
+
+    // Hiển thị kết quả
+    cout << "Phép tính 1: S1 = " << S1 << endl;
+    cout << "Phép tính 2: S2 = " << S2 << endl;
+    cout << "Phép tính 3: S3 = " << S3 << endl;
+    cout << "Phép tính 4: S4 = " << S4 << endl;
+
+    return 0;
+}
+
---
+++ PreIT-6-VeHinh-While/PreIT-6-VeHinh-While.cpp
@@ -0,0 +1,252 @@
+﻿// PreIT-6-LuyenTap.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	// Hình 10 : 
+	cout << "Hinh 10 : 2 vong While : " << endl;
+	int n;
+	cout << "Nhap canh : ";
+	cin >> n;
+
+	int i = 0;
+	while (i < n)
+	{
+		int j = 0;
+		while (j < n)
+		{
+			cout << "* ";
+			j++;
+		}
+		cout << endl;
+		i++;
+	}
+
+	// ví dụ 2 : Chỉ cần 1 vòng lặp
+	cout << "Hinh 10 : 1 vong While : " << endl;
+	int nn = n * n;
+	i = 1;
+	while (i <= nn)
+	{
+		cout << "* ";
+		if (i % n == 0)
+		{
+			cout << endl;
+		}
+		i++;
+	}
+
+	cout << endl;
+
+	// Hình 11 : 
+	cout << "Hinh 11 : dung 2 vong While : " << endl;
+	i = 0;
+	while (i < n)
+	{
+		int j = 0;
+		while (j < n)
+		{
+			if (i == 0 || i == n - 1 || j == 0 || j == n - 1)
+			{
+				cout << "* ";
+			}
+			else
+			{
+				cout << "  ";
+			}
+			j++;
+		}
+		i++;
+		cout << endl;
+	}
+
+	// ví dụ : chỉ dùng 1 while
+	cout << "Hinh 11 : dung 1 vong While : " << endl;
+	while (i < nn)
+	{
+		int hangNgang = i / n;
+		int hangDoc = i % n;
+
+		if (hangNgang == 0 || hangNgang == n - 1 || hangDoc == 0 || hangDoc == n - 1)
+		{
+			cout << "* ";
+		}
+
+		else
+		{
+			cout << "  ";
+		}
+
+		if (hangDoc == n - 1)
+		{
+			cout << endl;
+		}
+		i++;
+	}
+	cout << endl;
+
+	// hình 12 : 
+	cout << "hinh 12 : dung 2 vong While :  " << endl;
+	int chieuDai, chieuRong;
+	cout << "Nhap chieu dai : ";
+	cin >> chieuDai;
+	cout << "Nhap chieu rong : ";
+	cin >> chieuRong;
+
+	i = 0;
+	while (i < chieuDai)
+	{
+		int j = 0;
+		while (j < chieuRong)
+		{
+			cout << "* ";
+			j++;
+		}
+		cout << endl;
+		i++;
+	}
+	cout << endl;
+
+	// Hình 12 : chỉ dùng 1 vòng While
+	cout << "Hinh 12 : chi dung 1 vong While" << endl;
+
+	int count = 0;
+
+	while (count < chieuDai * chieuRong)
+	{
+		cout << "* ";
+		if ((count + 1) % chieuRong == 0)
+		{
+			cout << endl;
+		}
+		count++;
+	}
+	cout << endl;
+
+	// Hình 13 :
+	cout << " Hinh 13 : dung 2 vong While :  " << endl;
+	i = 0;
+	while (i < chieuRong)
+	{
+		int j = 0;
+		while (j < chieuDai)
+		{
+			if (i == 0 || i == chieuRong - 1 || j == 0 || j == chieuDai - 1)
+			{
+				cout << "* ";
+			}
+			else
+			{
+				cout << "  ";
+			}
+			j++;
+		}
+		cout << endl;
+		i++;
+	}
+
+	// Hình 13 : chỉ dùng 1 vòng While
+	cout << "Hinh 13 : chi dung 1 vong While" << endl;
+
+	int hang = 1;
+	int cot = 1;
+
+	while (hang <= chieuRong)
+	{
+		if (hang == 1 || cot == 1 || hang == chieuRong || cot == chieuDai)
+		{
+			cout << "* ";
+		}
+		else
+		{
+			cout << "  ";
+		}
+		cot++;
+		if (cot > chieuDai)
+		{
+			cout << endl;
+			hang++;
+			cot = 1;
+		}
+	}
+	
+	// Hình 14 : 
+	cout << "Hinh 14 : dung 2 vong While : " << endl;
+	int chieuCao;
+	cout << "Nhap chieu cao : ";
+	cin >> chieuCao;
+
+	hang = 1;
+
+	while (hang <= chieuCao) 
+	{
+		int cot = 1;
+		while (cot <= hang) 
+		{
+			cout << "* ";
+			cot++;
+		}
+		cout << endl;
+		hang++;
+	}
+
+	// Hình 15 :
+	cout << "Hinh 15 : dung 2 vong While : " << endl;
+	hang = 1;
+	while (hang <= chieuCao) 
+	{
+		cot = 1;
+		while (cot <= hang) 
+		{
+			if (cot == hang || cot == 1 || hang == chieuCao) 
+			{
+				cout << "* ";
+			}
+			else {
+				cout << "  ";
+			}
+			cot++;
+		}
+		cout << endl;
+		hang++;
+	}
+
+	// Hình 16 : 
+	cout << "Hinh 16 : dung 2 vong While : " << endl;
+
+	hang = 1;
+	while (hang <= chieuCao) 
+	{
+		cot = 1;
+		while (cot <= hang) 
+		{
+			cout << cot << " ";
+			cot++;
+		}
+		cout << endl;
+		hang++;
+	}
+
+	// Hình 17 :
+	cout << "Hinh 17 : dung 2 vong While : " << endl;
+	hang = chieuCao;
+	while (hang >= 1) 
+	{
+		cot = 1;
+		while (cot <= hang) 
+		{
+			cout << cot << " ";
+			cot++;
+		}
+		cout << endl;
+		hang--;
+	}
+
+	return 0;
+}
+
+
