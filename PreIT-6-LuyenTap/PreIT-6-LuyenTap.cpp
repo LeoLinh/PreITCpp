a4ebd06132d05c53423e6075a63e57332b7a204a
pc <pc@DESKTOP-KJBOUAL>
2024-05-04 21:20:21 +0700

    update

--- PreIT-6-LuyenTap/PreIT-6-LuyenTap.cpp
+++ PreIT-6-LuyenTap/PreIT-6-LuyenTap.cpp
@@ -7,170 +7,44 @@ using namespace std;
 
 int main()
 {
-	// Hình 10 : 
-	int n;
-	cout << "Nhap canh : ";
-	cin >> n;
+	int a, b, c, x;
+	cout << "Nhap gia tri a : ";
+	cin >> a;
+	cout << "Nhap gia tri b : ";
+	cin >> b;
+	cout << "Nhap gia tri c : ";
+	cin >> c;
 
-	int i = 0;
-	/*while (i < n)
-	{
-		int j = 0;
-		while (j < n)
-		{
-			cout << "* ";
-			j++;
-		}
-		cout << endl;
-		i++;
-	}*/
+	/*c = a;
+	a = b;
+	b = c;*/
+	
+	//a = a + b;
+	//b = a - b;
 
-	// ví dụ 2 : Chỉ cần 1 vòng lặp
-	int nn = n * n;
-	i = 1;
-	while (i <= nn)
+	if (a > b)
 	{
-		cout << "* ";
-		if (i % n == 0)
-		{
-			cout << endl;
-		}
-		i++;
+		x = a;
+		a = b;
+		b = x;
 	}
-
-	cout << endl;
-
-	// Hình 11 : 
-
-	i = 0;
-	/*while (i < n)
+	if (a > c)
 	{
-		int j = 0;
-		while (j < n)
-		{
-			if (i == 0 || i == n - 1 || j == 0 || j == n - 1)
-			{
-				cout << "* ";
-			}
-			else
-			{
-				cout << "  ";
-			}
-			j++;
-		}
-		i++;
-		cout << endl;
-	}*/
-
-	// ví dụ : chỉ dùng 1 while
-	while (i < nn)
-	{
-		int hangNgang = i / n;
-		int hangDoc = i % n;
-
-		if (hangNgang == 0 || hangNgang == n - 1 || hangDoc == 0 || hangDoc == n - 1)
-		{
-			cout << "* ";
-		}
-
-		else
-		{
-			cout << "  ";
-		}
-
-		if (hangDoc == n - 1)
-		{
-			cout << endl;
-		}
-		i++;
+		x = a;
+		a = c;
+		c = x;
 	}
-	cout << endl;
-
-	// hình 12 : 
-	cout << "hinh 12 : " << endl;
-	int chieuDai, chieuRong;
-	cout << "Nhap chieu dai : ";
-	cin >> chieuDai;
-	cout << "Nhap chieu rong : ";
-	cin >> chieuRong;
-
-	i = 0;
-	while (i < chieuDai)
+	if (b > c)
 	{
-		int j = 0;
-		while (j < chieuRong)
-		{
-			cout << "* ";
-			j++;
-		}
-		cout << endl;
-		i++;
+		x = b;
+		b = c;
+		c = x;
 	}
-	cout << endl;
+	
 
-	// Hình 13 :
-	cout << " Hinh 13 : " << endl;
-	i = 0;
-	while (i < chieuDai)
-	{
-		int j = 0;
-		while (j < chieuRong)
-		{
-			if (i == 0 || i == chieuDai - 1 || j == 0 || j == chieuRong - 1)
-			{
-				cout << "* ";
-			}
-			else
-			{
-				cout << "  ";
-			}			
-			j++;	
-		}
-		cout << endl;
-		i++;
-	}
+	cout << "=> day so la  : " << a << " ; " << b << " ; " << c << endl;
 
-	// Hình 12 : chỉ dùng 1 vòng While
-	cout << "Hinh 12 : chi dung 1 vong While" << endl;
 
-	int count = 0;
-
-	while (count < chieuDai * chieuRong)
-	{
-		cout << "* ";
-		if ((count + 1) % chieuRong == 0)
-		{
-			cout << endl;
-		}
-		count++;
-	}
-	cout << endl;
-
-	// Hình 13 : chỉ dùng 1 vòng While
-	cout << "Hinh 13 : chi dung 1 vong While" << endl;
-
-	while (i < chieuDai * chieuRong)
-	{
-		int CD = i / chieuDai;
-		int CR = i % chieuRong;
-
-		if (CD == 0 || CD == chieuDai - 1 || CR == 0 || CR == chieuRong - 1)
-		{
-			cout << "* ";
-		}
-
-		else
-		{
-			cout << "  ";
-		}
-
-		if (CD == chieuDai - 1)
-		{
-			cout << endl;
-		}
-		i++;
-	}
-	cout << endl;
 
 
 
---
+++ PreIT-7-HocTrenLop/PreIT-7-HocTrenLop.cpp
@@ -0,0 +1,111 @@
+﻿// PreIT-6-LuyenTap.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	//int a, b, c, x;
+	//cout << "Nhap gia tri a : ";
+	//cin >> a;
+	//cout << "Nhap gia tri b : ";
+	//cin >> b;
+	//cout << "Nhap gia tri c : ";
+	//cin >> c;
+
+	///*c = a;
+	//a = b;
+	//b = c;*/
+
+	////a = a + b;
+	////b = a - b;
+
+	//if (a > b)
+	//{
+	//	x = a;
+	//	a = b;
+	//	b = x;
+	//}
+	//if (a > c)
+	//{
+	//	x = a;
+	//	a = c;
+	//	c = x;
+	//}
+	//if (b > c)
+	//{
+	//	x = b;
+	//	b = c;
+	//	c = x;
+	//}
+	//cout << "=> day so la  : " << a << " ; " << b << " ; " << c << endl;
+	//////////////////////////////////////////////////////////////
+
+
+	// in các số chia hết cho 3 :
+
+	/*int num[10];
+
+	for (int i = 0; i < 10; i++)
+	{
+		cout << "Nhap so " << i << " = ";
+		cin >> num[i];
+	}
+
+	cout << "day so : ";
+
+	for (int i = 0; i < 10; i++)
+	{
+		cout << num[i] << "; ";
+	}
+
+	cout << endl << "Cac so chia het cho 3 : ";
+	for (int i = 0; i < 10; i++)
+	{
+		if ((num[i] % 3) == 0)
+		{
+			cout << num[i] << " ; ";
+		}
+	}*/
+
+	// in số đầu tiên chia hết cho 4 :
+	
+	/*bool isExist = false;
+	int i;
+	for (i = 0; i < 10; i++)
+	{
+		if ((num[i] % 4) == 0)
+		{
+			cout << endl << "So dau tien chia het cho 4 : " << num[i];
+			isExist = true;
+			break;
+		}
+	}
+	if (i >= 10)
+	{
+		cout << endl << "=> Khong co so nao trong mang chia het cho 4";
+	}*/
+
+
+	// Yêu cầu 1: Liệt kê vị trí và giá trị các phần tử là số chẵn.
+	int num[] = { 10, 25, -4, 32, 63, 81, 19, -24, 13, 18, 45, 12, 72, 42, -6 };
+
+	for (int i = 0; i < 15; i++)
+	{
+		if ((num[i] % 2) == 0)
+		{
+			cout << endl << "So chan : " << num[i] << " vi tri : " << i;	
+		}
+	}
+
+
+
+
+
+
+	return 0;
+}
+
+
